63e61af6591552d9d4dd368dcdb79eafe3cfacc6
Shakti Gurung <mr.shakti.gurung@gmail.com>
2019-12-24 21:08:38 +1100

    Stripe Payment System

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure();
+require('dotenv').config();
 
 function App() {
+  const [product] = React.useState({
+    name: "Tesla Roadster",
+    price: 64998.67,
+    description: "Cool car"
+  });
+
+  async function handleToken(token, addresses) {
+    const response = await axios.post(
+      "/checkout",
+      { token, product }
+    );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast.success("Success! Check email for details");
+    } else {
+      toast.error("Something went wrong");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="product">
+        <h1>{product.name}</h1>
+        <h3>On Sale · ${product.price}</h3>
+      </div>
+      <StripeCheckout
+        stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
+        token={handleToken}
+        amount={product.price * 100}
+        name="Tesla Roadster"
+        billingAddress
+        shippingAddress
+      />
     </div>
   );
 }
 
 export default App;
+
